bc0b56a9865eef046812e615a18c586bcf5bf9e8
vishal vashishta <vishdhanu@gmail.com>
2020-02-15 13:24:14 +0530

    Moved truncate function to helper file

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -148,6 +148,16 @@ if ( ! function_exists('participantData'))
     }   
 }
 
+if ( ! function_exists('truncate'))
+{
+    function truncate($str, $len) {
+        $tail = max(0, $len-10);
+        $trunk = substr($str, 0, $tail);
+        $trunk .= strrev(preg_replace('~^..+?[\s,:]\b|^...~', '...', strrev(substr($str, $tail, $len-$tail))));
+        return $trunk;
+    }  
+}
+
 
 
 ?>
--- application/views/admin/news/news.php
+++ application/views/admin/news/news.php
@@ -461,12 +461,3 @@
 
 
 </script>
-
-<?
-function truncate($str, $len) {
-  $tail = max(0, $len-10);
-  $trunk = substr($str, 0, $tail);
-  $trunk .= strrev(preg_replace('~^..+?[\s,:]\b|^...~', '...', strrev(substr($str, $tail, $len-$tail))));
-  return $trunk;
-}
-?>
